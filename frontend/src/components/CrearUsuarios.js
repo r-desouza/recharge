d55072b9dcab342883d0b7a23238d61a038ff91c
rdesouza <worstrivenla@gmail.com>
2023-06-01 23:19:03 -0300

    POST METHOD ATTEMPT

--- frontend/src/components/CrearUsuarios.js
+++ frontend/src/components/CrearUsuarios.js
@@ -1,67 +1,132 @@
-import React from "react";
+import { set } from "mongoose";
+import React, { useState } from "react";
 
 const CrearUsuarios = () => {
+  const valorInicial = {
+    nombre: "",
+    apellido: "",
+    edad: 18,
+    telefono: 0,
+    correo: "",
+  };
+
+  const [usuario, setUsuario] = useState(valorInicial);
+
+  const capturarDatos = (e) => {
+    const { name, value } = e.target;
+    setUsuario({ ...usuario, [name]: value });
+  };
+
+  const guardarDatos = (e) => {
+    e.preventDefault();
+
+    // POST logic
+    const newUser = {
+      nombre: usuario.nombre,
+      apellido: usuario.apellido,
+      edad: usuario.edad,
+      telefono: usuario.telefono,
+      correo: usuario.correo,
+    };
+
+
+      try {
+        const response =  fetch("http://localhost:4000/api/usuarios", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newUser),
+        })
+        //.then(console.log(response.json));
+
+        const result = response.json();
+        console.log("Success:", result);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    
+
+
+
+    setUsuario({ ...valorInicial });
+  };
+
   return (
     <div className="col-md-6 offset-md-3">
       <div className="card card-body">
-        <form>
-          <h2 className='text-center mb-3'>Crear Usuario</h2>
-        <div className="mb-3">
-          <label>Nombre:</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="ingresar el nombre del usuario"
-            required
-          />
-        </div>
+        <form onSubmit={guardarDatos}>
+          <h2 className="text-center mb-3">Crear Usuario</h2>
+          <div className="mb-3">
+            <label>Nombre:</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="ingresar el nombre del usuario"
+              required
+              name="nombre"
+              value={usuario.nombre}
+              onChange={capturarDatos}
+            />
+          </div>
 
-        <div className="mb-3">
-          <label>Apellido:</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="ingresar el apellido del usuario"
-            required
-          />
-        </div>
+          <div className="mb-3">
+            <label>Apellido:</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="ingresar el apellido del usuario"
+              required
+              name="apellido"
+              value={usuario.apellido}
+              onChange={capturarDatos}
+            />
+          </div>
 
-        <div className="mb-3">
-          <label>Edad:</label>
-          <input
-            type="number"
-            className="form-control"
-            placeholder="ingresar la edad del usuario"
-            required
-          />
-        </div>
-        
-        <div className="mb-3">
-          <label>Telefono:</label>
-          <input
-            type="number"
-            className="form-control"
-            placeholder="ingresar el telefono del usuario"
-            required
-          />
-        </div>
+          <div className="mb-3">
+            <label>Edad:</label>
+            <input
+              type="number"
+              className="form-control"
+              placeholder="ingresar la edad del usuario"
+              required
+              name="edad"
+              value={usuario.edad}
+              onChange={capturarDatos}
+            />
+          </div>
 
-        <div className="mb-3">
-          <label>Correo:</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="ingresar el correo del usuario"
-            required
-          />
-        </div>
+          <div className="mb-3">
+            <label>Telefono:</label>
+            <input
+              type="number"
+              className="form-control"
+              placeholder="ingresar el telefono del usuario"
+              required
+              name="telefono"
+              value={usuario.telefono}
+              onChange={capturarDatos}
+            />
+          </div>
 
-        <button className='btn btn-primary form-control'>
-          Guardar Usuario
-        </button>
+          <div className="mb-3">
+            <label>Correo:</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="ingresar el correo del usuario"
+              required
+              name="correo"
+              value={usuario.correo}
+              onChange={capturarDatos}
+            />
+          </div>
+
+          <button className="btn btn-primary form-control">
+            Guardar Usuario
+          </button>
         </form>
       </div>
-      
     </div>
   );
 };
