d310a56d86354a36a5ccb6ff3d8d1c11aa7bb24b
rdesouza <worstrivenla@gmail.com>
2023-06-01 22:29:56 -0300

    added crear usuario screen

--- frontend/src/components/CrearUsuarios.js
+++ frontend/src/components/CrearUsuarios.js
@@ -1,11 +1,69 @@
-import React from 'react'
+import React from "react";
 
 const CrearUsuarios = () => {
   return (
-    <div>
-      crear usuario
-    </div>
-  )
-}
+    <div className="col-md-6 offset-md-3">
+      <div className="card card-body">
+        <form>
+          <h2 className='text-center mb-3'>Crear Usuario</h2>
+        <div className="mb-3">
+          <label>Nombre:</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="ingresar el nombre del usuario"
+            required
+          />
+        </div>
 
-export default CrearUsuarios
+        <div className="mb-3">
+          <label>Apellido:</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="ingresar el apellido del usuario"
+            required
+          />
+        </div>
+
+        <div className="mb-3">
+          <label>Edad:</label>
+          <input
+            type="number"
+            className="form-control"
+            placeholder="ingresar la edad del usuario"
+            required
+          />
+        </div>
+        
+        <div className="mb-3">
+          <label>Telefono:</label>
+          <input
+            type="number"
+            className="form-control"
+            placeholder="ingresar el telefono del usuario"
+            required
+          />
+        </div>
+
+        <div className="mb-3">
+          <label>Correo:</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="ingresar el correo del usuario"
+            required
+          />
+        </div>
+
+        <button className='btn btn-primary form-control'>
+          Guardar Usuario
+        </button>
+        </form>
+      </div>
+      
+    </div>
+  );
+};
+
+export default CrearUsuarios;
