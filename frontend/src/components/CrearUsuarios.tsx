e289e0106538a6b5f6dac50334725f5e464d3b8e
AthenaiProg <rodrigoalmiron997@hotmail.com>
2023-06-05 01:05:33 -0300

    v3

--- frontend/src/components/CrearUsuarios.tsx
+++ frontend/src/components/CrearUsuarios.tsx
@@ -13,7 +13,6 @@ const CrearUsuarios = () => {
 
   const [usuario, setUsuario] = useState(valorInicial);
 
-
   const capturarDatos = (e) => {
     const { name, value } = e.target;
     setUsuario((prev) => ({ ...prev, [name]: value }));
@@ -39,9 +38,9 @@ const CrearUsuarios = () => {
     <div className="col-md-6 offset-md-3">
       <div className="card card-body">
         <form onSubmit={guardarDatos}>
-          <h2 className="text-center mb-3">Crear Usuario</h2>
+          <h2 className="text-center mb-3">Registrarse</h2>
           <div className="mb-3">
-            <label>Nombre:</label>
+            <label>Nombre</label>
             <input
               type="text"
               className="form-control"
@@ -54,7 +53,7 @@ const CrearUsuarios = () => {
           </div>
 
           <div className="mb-3">
-            <label>Apellido:</label>
+            <label>Apellido</label>
             <input
               type="text"
               className="form-control"
@@ -67,7 +66,7 @@ const CrearUsuarios = () => {
           </div>
 
           <div className="mb-3">
-            <label>Edad:</label>
+            <label>Edad</label>
             <input
               type="number"
               className="form-control"
@@ -80,11 +79,11 @@ const CrearUsuarios = () => {
           </div>
 
           <div className="mb-3">
-            <label>Telefono:</label>
+            <label>Telefono</label>
             <input
               type="number"
               className="form-control"
-              placeholder="ingresar el telefono del usuario"
+              placeholder="ingresar tu telefono"
               required
               name="telefono"
               value={usuario.telefono}
@@ -93,11 +92,11 @@ const CrearUsuarios = () => {
           </div>
 
           <div className="mb-3">
-            <label>Correo:</label>
+            <label>Correo</label>
             <input
               type="text"
               className="form-control"
-              placeholder="ingresar el correo del usuario"
+              placeholder="ingresa tu correo"
               required
               name="correo"
               value={usuario.correo}
@@ -106,7 +105,7 @@ const CrearUsuarios = () => {
           </div>
 
           <button className="btn btn-primary form-control">
-            Guardar Usuario
+            Registrarse
           </button>
         </form>
        
