07ac20c92d6c6f51ca0faea39277e7caa61c8044
rdesouza <worstrivenla@gmail.com>
2023-06-06 09:40:15 -0300

    update frontline

--- frontend/src/components/LogIn.tsx
+++ frontend/src/components/LogIn.tsx
@@ -1,48 +1,22 @@
 import React, { useEffect, useState } from "react";
-
-
+import Form from "react-bootstrap/Form";
 
 const LogIn = () => {
+  const valorInicial = {
+    nombre: "",
+    password: "",
+  };
 
-    const valorInicial = {
-        nombre: "",
-        apellido: "",
-        correo: "",
-      };
-      
-      const [usuario, setUsuario] = useState(valorInicial);
+  const [usuario, setUsuario] = useState(valorInicial);
 
   return (
     <div className="col-md-4 offset-md-4">
       <div className="card card-body">
-        <form >
-          <h2 className="text-center mb-3">Welcome back</h2>
-          <div className="mb-3">
-            <label>Nombre</label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder=""
-              required
-              name="nombre"
-              value={usuario.nombre}
-            />
-          </div>
+        <form>
+          <h2 className="text-center mb-3">Welcome back!</h2>
 
           <div className="mb-3">
-            <label>Apellido</label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder=""
-              required
-              name="apellido"
-              value={usuario.apellido}
-            />
-          </div>
-
-          <div className="mb-3">
-            <label>Correo</label>
+            <label>Email</label>
             <input
               type="text"
               className="form-control"
@@ -53,6 +27,21 @@ const LogIn = () => {
             />
           </div>
 
+          <div className="mb-3">
+            <label>Password</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder=""
+              required
+              name="apellido"
+              value={usuario.apellido}
+            />
+          </div>
+
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Remember me" />
+          </Form.Group>
           <button className="btn btn-primary form-control">Log in</button>
         </form>
       </div>
--- frontend/src/components/SendRecharge.tsx
+++ frontend/src/components/SendRecharge.tsx
@@ -1,10 +1,41 @@
+import { Card, Form, InputGroup } from "react-bootstrap";
+
 const SendRecharge = () => {
-    return (
-        <div >
-          <h1 className="text-white text-center mb-3 mt-4">Recharge Mobiles Online
-</h1>
-        </div>
-      );
-}
+  return (
+    <div>
+      <div>
+        <Card className="col-md-4 offset-md-4">
+          <Card.Title className="text-center mb-3 mt-4">
+            Recharge Mobiles Online
+          </Card.Title>
+          <Card.Body>
+            <Form>
+              <Form.Group className="mb-3" controlId="country">
+                <Form.Label>Country</Form.Label>
+                <Form.Select aria-label="Default select example">
+                  <option value="1">Argentina</option>
+                  <option value="2">Nigeria</option>
+                </Form.Select>
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="phoneNumber">
+                <Form.Label>Phone Number</Form.Label>
+                <Form.Control type="tel" placeholder="Enter phone number" />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="amount">
+                <Form.Label>Amount</Form.Label>
+                <InputGroup className="mb-3">
+                  <InputGroup.Text>$</InputGroup.Text>
+                  <Form.Control aria-label="Dollar amount (with dot and two decimal places)" />
+                </InputGroup>
+              </Form.Group>
+            </Form>
+          </Card.Body>
+        </Card>
+      </div>
+    </div>
+  );
+};
 
 export default SendRecharge;
