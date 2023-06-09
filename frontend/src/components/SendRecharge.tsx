410181cd2bbac63b440ef5acb8ef85961a80ae4a
rdesouza <worstrivenla@gmail.com>
2023-06-09 12:56:28 -0300

    prefix + update recharge

--- frontend/src/components/SendRecharge.tsx
+++ frontend/src/components/SendRecharge.tsx
@@ -1,39 +1,40 @@
 import { Card, Form, InputGroup, Button } from "react-bootstrap";
 import { all, filter } from "mcc-mnc-list";
 import { useState, useEffect } from "react";
-
+import prefixData from "../prefix-phone.json"
+import { collection, addDoc} from 'firebase/firestore'
+import { db } from '../firebase'
+import { UserAuth } from "../context/AuthContext";
 
 //PENDIENTE: MANEJAS LOS NULL DE ALGUNA FORMA
 
 const SendRecharge = () => {
   const list = all();
-
   const uniqueCountries2 = new Map(list.map(pais => [pais.countryName ,pais.countryCode]))
-
   const uniqueCountries = [...uniqueCountries2];
-
+  const { user } = UserAuth();
+  let coCode = ' ';
   uniqueCountries.sort();
 
   const [selectedCountry, setSelectedCountry] = useState("");
   const [selectedBrand, setSelectedBrand] = useState("");
+  const [selectedPrefix, setSelectedPrefix] = useState("");
+  const [phone, setPhone] = useState(""); 
+  const [amount, setAmount] = useState("");
   const [uniqueOps, setUniqueOps] = useState([]);
 
   const setSelectedCountryHandler = (e) => {
-
     const [key, value] = e.target.value.split(',')
-
     setSelectedCountry({ [key]: value });
-
   };
 
   const setSelectedBrandHandler = (e) => {
-
     setSelectedBrand(e.target.value);
-
   };
 
   useEffect(() => {
-    console.log(Object.values(selectedCountry)[0])
+    coCode = Object.values(selectedCountry)[0];
+    setSelectedPrefix(prefixData[coCode])
     const ops = filter({
       statusCode: "Operational",
       countryCode: Object.values(selectedCountry)[0],
@@ -42,10 +43,35 @@ const SendRecharge = () => {
     const uniqueOperators = [...new Set(ops.map((option) => option.brand))];
     uniqueOperators.sort();
     setUniqueOps(uniqueOperators);
+    setSelectedBrand(uniqueOperators[0])
     }
-
   }, [selectedCountry]);
 
+  // SUBIR RECARGA A LA DB
+
+  const submitRecharge = async (e) => {
+    e.preventDefault();
+    console.log(selectedBrand)
+    try{ 
+
+      const recarga = {
+        companiaRecarga: selectedBrand,
+        date: Date.now(),
+        estadoRecarga: "Pendiente",
+        idComprador: user.uid,
+        montoRecarga: amount,
+        numeroTelefono: '+' + selectedPrefix + ' '+ phone,
+        paisRecarga: Object.keys(selectedCountry)[0]
+      }
+
+      await addDoc( collection(db, 'recargas'), recarga)
+
+      console.log(recarga)
+
+    } catch (e) {
+      console.log(e.message)
+    }
+  }
 
   return (
     <div>
@@ -55,7 +81,7 @@ const SendRecharge = () => {
             Recharge Mobiles Online
           </Card.Title>
           <Card.Body>
-            <Form>
+            <Form onSubmit={submitRecharge}>
               <Form.Group className="mb-3" controlId="country">
                 <Form.Label>Country</Form.Label>
                 <Form.Select
@@ -83,14 +109,15 @@ const SendRecharge = () => {
 
               <Form.Group className="mb-3" controlId="phoneNumber">
                 <Form.Label>Phone Number</Form.Label>
-                <Form.Control type="tel" placeholder="Enter phone number" />
+                <InputGroup.Text>+{selectedPrefix}</InputGroup.Text>
+                <Form.Control onChange={(e) => setPhone(e.target.value)} type="tel" placeholder="Enter phone number" />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="amount">
                 <Form.Label>Amount</Form.Label>
                 <InputGroup className="mb-3">
                   <InputGroup.Text>$</InputGroup.Text>
-                  <Form.Control aria-label="Dollar amount (with dot and two decimal places)" />
+                  <Form.Control onChange={(e) => setAmount(e.target.value)} aria-label="Dollar amount (with dot and two decimal places)" />
                 </InputGroup>
               </Form.Group>
 
