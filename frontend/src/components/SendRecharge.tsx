cdff37533a608d5a5d370cba457ccfae43fa0cc6
rdesouza <worstrivenla@gmail.com>
2023-06-15 03:30:11 -0300

    Revert "payment id added"
    This reverts commit eb168614c0c746ec5f389d239b123eb5b00f2479.

--- frontend/src/components/SendRecharge.tsx
+++ frontend/src/components/SendRecharge.tsx
@@ -27,7 +27,6 @@ interface RechargeData {
   phone: string;
   status?: string;
   orderId?: string;
-  paymentId?: string;
 }
 
 const SendRecharge = (props: SendRechargeProps) => {
@@ -99,7 +98,6 @@ const SendRecharge = (props: SendRechargeProps) => {
         paisRecarga: Object.keys(data.selectedCountry)[0],
         paypalOrderID: data.orderId,
         paypalOrderStatus: data.status,
-        paypalPaymentID: data.paymentId,
       };
 
       showToast("Successs", "Recharge submitted successfully");
