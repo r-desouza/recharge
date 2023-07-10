005dc207e2b324c427667c245052f4728547022b
AthenaiProg <rodrigoalmiron997@hotmail.com>
2023-07-10 17:21:08 -0300

    removed useless lines

--- frontend/src/components/AdminDashboard.tsx
+++ frontend/src/components/AdminDashboard.tsx
@@ -221,7 +221,7 @@ export const AdminDashboard = (props: AccountProps) => {
               </div>
             </div>
 
-            <table className="caption-top bg-white rounded mt-2 table-sm">
+            <table className="caption-top bg-white rounded mt-2 table-sm table table-striped">
               <caption className="text-white fs-4">Orders </caption>
 
               <caption>
@@ -269,13 +269,6 @@ export const AdminDashboard = (props: AccountProps) => {
                   onChange={(e) => handleChangeSearch(e.target.value)}
                 />
 
-                {/* <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Search by UserID or PaypalOrderID"
-                  required
-                  onChange={(e) => handleChangeSearch(e.target.value)}
-                /> */}
               </caption>
 
               <thead>
@@ -328,11 +321,11 @@ export const AdminDashboard = (props: AccountProps) => {
                         {convertDate(recarga.date)}
                       </td>
                       <td > 
-                      {/* <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">{recarga.estadoRecarga}<svg className="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button> */}
+  
 
                         <Dropdown  as={ButtonGroup} className="col-12">
 
-                          <Button variant={setButtonVariant(recarga.estadoRecarga)}>
+                          <Button variant={setButtonVariant(recarga.estadoRecarga)} >
                             {recarga.estadoRecarga}
                           </Button>
 
